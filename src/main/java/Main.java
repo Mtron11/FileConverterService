8c0bcf6831776ed8b08608e6333316b27d427058
Mtron11 <oddintsovv@yandex.ru>
2023-11-02 18:53:22 +0300

    Добавлена запись в файл json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,10 @@ import XML.XMLToJSONConverter;
 public class Main {
     public static void main(String[] args) throws Exception {
         String xmlFilePath = "C:\\Users\\Mtron\\Desktop\\Monsters.xml";
+        String jsonFilePath = "C:\\Users\\Mtron\\Desktop\\Monsters.json";
+
         ReadXML xmlParser = new ReadXML(xmlFilePath);
         XMLToJSONConverter converter = new XMLToJSONConverter(xmlParser);
-        String json = converter.convertToJSON();
-        System.out.println(json);
+        converter.convertAndWriteToJSONFile(jsonFilePath);
     }
 }
--- src/main/java/XML/XMLToJSONConverter.java
+++ src/main/java/XML/XMLToJSONConverter.java
@@ -3,6 +3,9 @@ import Read.ReadXML;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.io.File;
+import java.util.List;
+
 public class XMLToJSONConverter {
     private ReadXML xmlParser;
 
@@ -10,9 +13,10 @@ public class XMLToJSONConverter {
         this.xmlParser = xmlParser;
     }
 
-    public String convertToJSON() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        return objectMapper.writeValueAsString(xmlParser.getMonsters());
+    public String convertAndWriteToJSONFile(String jsonFilePath) throws Exception {
+            List<Monster> monsters = xmlParser.getMonsters();
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+            return objectMapper.writeValueAsString(monsters);
     }
 }
